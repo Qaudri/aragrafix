cbcdf20d021062d01e046c54553dfaaf3b03ae52
Qaudri <aquadirmuhammad@gmail.com>
2022-03-30 20:30:30 +0100

    chore: page outline created

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <Tutorial/>
+  <body>
+    <section class="h-screen container mx-auto flex items-center justify-center">
+      <div class="flex relative">
+        <div class="w-1/2">
+          <h1 class="font-black text-7xl text-gray-500">Hello</h1>
+        </div>
+      </div>
+    </section>
+  </body>
 </template>
 
 <script>
