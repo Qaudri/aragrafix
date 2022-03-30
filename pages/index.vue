3b65b935b1bfaa66a3c22c00b8512463d6147777
Qaudri <aquadirmuhammad@gmail.com>
2022-03-30 20:33:37 +0100

    chore: image added

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <template>
   <body>
     <section class="h-screen container mx-auto flex items-center justify-center">
-      <div class="flex relative">
-        <div class="w-1/2">
+      <div class="flex">
+        <div class="w-1/2 relative">
           <h1 class="font-black text-7xl text-gray-500">Hello</h1>
+          <img :src=" require('@/assets/images/ope.png') " alt="">
         </div>
       </div>
     </section>
