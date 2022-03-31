5b8d4498fc9d9e910e06335c187d9559f4a0da63
Qaudri <aquadirmuhammad@gmail.com>
2022-03-31 12:39:36 +0100

    refactor: xl screen responsiveness

--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,13 @@
   <body>
     <section class="h-screen container mx-auto flex justify-center">
       <div class="lg:flex block">
-        <div class="w-full lg:w-1/2">
+        <div class="w-full lg:w-1/2 xl:w-2/5">
           <img :src=" require('@/assets/images/ope.png') " class="w-full lg:w-full pt-8" alt="">
         </div>
 
-        <div class="w-full lg:w-1/2 bottom-0 lg:flex lg:items-center lg:justify-center lg:h-screen">
+        <div class="w-full lg:w-1/2 bottom-0 lg:flex lg:items-center lg:justify-center lg:h-screen xl:w-3/5">
           <div>
-            <p class="block py-6 lg:py-0 font-normal md:text-lg text-base px-10 lg:text-xl lg:px-16 leading-snug lg:leading-tight">
+            <p class="block py-6 lg:py-0 font-normal md:text-lg text-base px-10 lg:text-xl lg:px-16 leading-snug lg:leading-tight xl:text-3xl xl:px-48">
               I'm Aramide Oluwaseun a Graphic Designer with 6+ years of experience in the management of the design process
               from conceptualization to implementation. Analytical and creative at solving business problems and management of lasting business relationships.
               <span class="block mt-4"> I am very curious individual who is always ready to learn new things and new ways to solve small and big problems, this curiosity allows me to discover, develop and create something out of nothing.
