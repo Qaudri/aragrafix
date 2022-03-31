09889d57be14ada3222b09b7bd738d5c6bb81455
Qaudri <aquadirmuhammad@gmail.com>
2022-03-31 21:44:47 +0100

    chore: vue slick carousel implemented

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,49 +1,75 @@
 <template>
-  <div class="bg-gray-600 h-screen py-10">
+  <div class="bg-gray-600 py-10">
     <div class="">
       <h1 class="text-4xl font-bold text-white flex justify-center mx-auto lg:text-6xl">Logo design</h1>
-      <div class="flex flex-nowrap overflow-x-hidden gap-4 mt-8 w-full slider">
-
-        <div class="bg-white relative " style="min-width: 400px;">
-          <img :src="require('@/assets/images/portfolio/logo design/IMG_20191017_222023_871.jpg')" class="w-full" />
-        </div>
+      <VueSlickCarousel :autoplay="true" :cssEase="ease" :pauseOnHover="true" :variableWidth="true" :arrows="false" :dots="false" :adaptiveHeight="true"
+       class="gap-4 mt-8 w-full ">
 
         <div class="bg-white relative" style="min-width: 400px;">
           <img :src="require('@/assets/images/portfolio/logo design/IMG_20191029_194327_299.jpg')" class="w-full" />
         </div>   
 
-        <div class="bg-white  relative" style="min-width: 400px;">
-          <img :src="require('@/assets/images/portfolio/logo design/IMG_20191017_222023_871.jpg')" class="w-full" />
-        </div>
-
-        <div class="bg-white  relative" style="min-width: 400px;">
+        <div class="bg-white relative" style="min-width: 400px;">
+          <img :src="require('@/assets/images/portfolio/logo design/IMG_20191029_194327_299.jpg')" class="w-full" />
+        </div>        
+        
+        <div class="bg-white relative" style="min-width: 400px;">
           <img :src="require('@/assets/images/portfolio/logo design/IMG_20191029_194327_299.jpg')" class="w-full" />
         </div>           
         
-        <div class="bg-white relative " style="min-width: 400px;">
-          <img :src="require('@/assets/images/portfolio/logo design/IMG_20191017_222023_871.jpg')" class="w-full" />
-        </div>
-
-        <div class="bg-white relative " style="min-width: 400px;">
+        <div class="bg-white relative" style="min-width: 400px;">
           <img :src="require('@/assets/images/portfolio/logo design/IMG_20191029_194327_299.jpg')" class="w-full" />
         </div>           
         
-        <div class="bg-white relative " style="min-width: 400px;">
-          <img :src="require('@/assets/images/portfolio/logo design/IMG_20191017_222023_871.jpg')" class="w-full" />
-        </div>
-
+        <div class="bg-white relative" style="min-width: 400px;">
+          <img :src="require('@/assets/images/portfolio/logo design/IMG_20191029_194327_299.jpg')" class="w-full" />
+        </div>           
+        
         <div class="bg-white relative" style="min-width: 400px;">
           <img :src="require('@/assets/images/portfolio/logo design/IMG_20191029_194327_299.jpg')" class="w-full" />
         </div>   
-      </div>
+      </VueSlickCarousel>
+    </div>
+
+    <div class="mt-20 ">
+      <h1 class="text-4xl font-bold text-white flex justify-center mx-auto lg:text-6xl">Flyer design</h1>
+      <VueSlickCarousel :autoplay="true" :cssEase="ease" :pauseOnHover="true" :variableWidth="true" :arrows="false" :dots="false" :adaptiveHeight="true" class="gap-4 mt-8 w-full ">
+
+        <div class="bg-white relative" style="width: 400px;">
+          <img :src="require('@/assets/images/portfolio/flyers/1st.jpg')" class="w-full" />
+        </div>   
+
+        <div class="bg-white relative" style="width: 400px;">
+          <img :src="require('@/assets/images/portfolio/flyers/3.jpg')" class="w-full" />
+        </div>        
+        
+        <div class="bg-white relative" style="width: 400px;">
+          <img :src="require('@/assets/images/portfolio/flyers/Charmaine_SourceSmart_eflyerdesign_flatII_corrected_Upwork.jpg')" class="w-full" />
+        </div>           
+        
+        <div class="bg-white relative" style="width: 400px;">
+          <img :src="require('@/assets/images/portfolio/flyers/Ife2.jpg')" class="w-full" />
+        </div>           
+        
+        <div class="bg-white relative" style="width: 400px;">
+          <img :src="require('@/assets/images/portfolio/flyers/Raboni.jpg')" class="w-full" />
+        </div>           
+        
+        <div class="bg-white relative" style="width: 400px;">
+          <img :src="require('@/assets/images/portfolio/flyers/WA_September.jpg')" class="w-full" />
+        </div>   
+      </VueSlickCarousel>
     </div>
 
   </div>
 </template>
 
 <script>
+  import VueSlickCarousel from 'vue-slick-carousel'
+  import 'vue-slick-carousel/dist/vue-slick-carousel.css'
 export default {
-
+  name: 'MyComponent',
+  components: { VueSlickCarousel },
 }
 </script>
 
