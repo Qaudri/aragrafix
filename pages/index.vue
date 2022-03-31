acf62e6296c8dc8907cc7b498710914d4d3f9827
Qaudri <aquadirmuhammad@gmail.com>
2022-03-31 11:53:46 +0100

    refactor: hero section text style changed

--- pages/index.vue
+++ pages/index.vue
@@ -7,11 +7,12 @@
         </div>
 
         <div class="w-full lg:w-1/2 bottom-0">
-          <p class="font-normal md:text-lg text-base px-10 lg:text-xl lg:flex  lg:items-center lg:h-screen">
+          <p class="block font-normal md:text-lg text-base px-10 lg:text-xl lg: lg:items-center lg:h-screen lg:px-16 leading-snug lg:leading-tight">
             I'm Aramide Oluwaseun a Graphic Designer with 6+ years of experience in the management of the design process
-            from conceptualization to implementation. Analytical and creative at solving bussiness problems and management of lasting business relationships.
-            I am very curious individual who is always ready to learn new things and new ways to solve small and big problems, this curiosity allows me to discover, develop and create something out of nothing.
-            I  love nature and every object I can find around me as well.
+            from conceptualization to implementation. Analytical and creative at solving business problems and management of lasting business relationships.
+            <span class="block mt-4"> I am very curious individual who is always ready to learn new things and new ways to solve small and big problems, this curiosity allows me to discover, develop and create something out of nothing.
+            I  love nature and every object I can find around me as well.</span>
+
           </p>
         </div>
       </div>
