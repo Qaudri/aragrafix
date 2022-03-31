d0e55b5036cbf5a44b53811ddb736e59dcc93b46
Qaudri <aquadirmuhammad@gmail.com>
2022-03-31 20:57:23 +0100

    portfolio images added on portfolio page

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,5 +1,42 @@
 <template>
-  <div class="bg-gray-600 h-screen">
+  <div class="bg-gray-600 h-screen py-10">
+    <div class="">
+      <h1 class="text-4xl font-bold text-white flex justify-center mx-auto lg:text-6xl">Logo design</h1>
+      <div class="flex flex-nowrap overflow-x-hidden gap-4 mt-8 w-full slider">
+
+        <div class="bg-white relative " style="min-width: 400px;">
+          <img :src="require('@/assets/images/portfolio/logo design/IMG_20191017_222023_871.jpg')" class="w-full" />
+        </div>
+
+        <div class="bg-white relative" style="min-width: 400px;">
+          <img :src="require('@/assets/images/portfolio/logo design/IMG_20191029_194327_299.jpg')" class="w-full" />
+        </div>   
+
+        <div class="bg-white  relative" style="min-width: 400px;">
+          <img :src="require('@/assets/images/portfolio/logo design/IMG_20191017_222023_871.jpg')" class="w-full" />
+        </div>
+
+        <div class="bg-white  relative" style="min-width: 400px;">
+          <img :src="require('@/assets/images/portfolio/logo design/IMG_20191029_194327_299.jpg')" class="w-full" />
+        </div>           
+        
+        <div class="bg-white relative " style="min-width: 400px;">
+          <img :src="require('@/assets/images/portfolio/logo design/IMG_20191017_222023_871.jpg')" class="w-full" />
+        </div>
+
+        <div class="bg-white relative " style="min-width: 400px;">
+          <img :src="require('@/assets/images/portfolio/logo design/IMG_20191029_194327_299.jpg')" class="w-full" />
+        </div>           
+        
+        <div class="bg-white relative " style="min-width: 400px;">
+          <img :src="require('@/assets/images/portfolio/logo design/IMG_20191017_222023_871.jpg')" class="w-full" />
+        </div>
+
+        <div class="bg-white relative" style="min-width: 400px;">
+          <img :src="require('@/assets/images/portfolio/logo design/IMG_20191029_194327_299.jpg')" class="w-full" />
+        </div>   
+      </div>
+    </div>
 
   </div>
 </template>
