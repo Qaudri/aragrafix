ad05955b9277f05a24f3fdc75e6deecf8dde0631
Qaudri <aquadirmuhammad@gmail.com>
2022-03-31 19:31:18 +0100

    refactor: portfolio section text comp

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <body>
+  <div>
     <section class="h-screen container mx-auto flex justify-center">
       <div class="lg:flex block">
         <div class="w-full lg:w-1/2 xl:w-2/5">
@@ -8,7 +8,7 @@
 
         <div class="w-full lg:w-1/2 bottom-0 lg:flex lg:items-center lg:justify-center lg:h-screen xl:w-3/5">
           <div>
-            <p class="block py-6 lg:py-0 font-normal md:text-lg text-base px-10 lg:text-xl lg:px-16 leading-snug lg:leading-tight 2xl:text-3xl xl:px-48">
+            <p class="block py-6 lg:py-0 font-normal text-xl text-base px-10 lg:px-16 leading-snug lg:leading-tight 2xl:text-3xl xl:px-48">
               I'm Aramide Oluwaseun a Graphic Designer with 6+ years of experience in the management of the design process
               from conceptualization to implementation. Analytical and creative at solving business problems and management of lasting business relationships.
               <span class="block mt-4"> I am very curious individual who is always ready to learn new things and new ways to solve small and big problems, this curiosity allows me to discover, develop and create something out of nothing.
@@ -21,14 +21,14 @@
 
     <section class="h-screen flex justify-center items-center">
       <div class="">
-        <h1 class="font-bold text-4xl xl:text-7xl text-gray-400 my-4">My Works</h1>
-        <p class="text-base xl:text-xl flex justify-center mx-auto">Check out some of my works below.</p>
+        <h1 class="font-bold text-6xl xl:text-7xl text-gray-400 my-4 flex justify-center mx-auto">My Works</h1>
+        <p class="text-xl flex justify-center mx-auto">Check out some of my works below.</p>
         <div class="grid grid-cols-3">
 
         </div>
       </div>
     </section>
-  </body>
+  </div>
 </template>
 
 <script>
