20f0531174178acc73c84e68c34d714641675464
Qaudri <aquadirmuhammad@gmail.com>
2022-04-01 14:27:53 +0100

    slider divs spacing

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -5,57 +5,57 @@
       <VueSlickCarousel :autoplay="true" :cssEase="'ease'" :pauseOnHover="true" :variableWidth="true" :arrows="false" :dots="false" :adaptiveHeight="true"
        class="gap-20 mt-8 w-full ">
 
-        <div class="bg-white relative" style="min-width: 400px;">
-          <img :src="require('@/assets/images/portfolio/logo design/IMG_20191029_194327_299.jpg')" class="w-full" />
+        <div class="bg-white relative mx-6" style="min-width: 400px;">
+          <img :src="require('@/assets/images/portfolio/logo design/IMG_20191029_194327_299.jpg')" class="" />
         </div>   
 
-        <div class="bg-white relative" style="min-width: 400px;">
-          <img :src="require('@/assets/images/portfolio/logo design/IMG_20191029_194327_299.jpg')" class="w-full" />
+        <div class="bg-white relative mx-6" style="min-width: 400px;">
+          <img :src="require('@/assets/images/portfolio/logo design/IMG_20191029_194327_299.jpg')" class="" />
         </div>        
         
-        <div class="bg-white relative" style="min-width: 400px;">
-          <img :src="require('@/assets/images/portfolio/logo design/IMG_20191029_194327_299.jpg')" class="w-full" />
+        <div class="bg-white relative mx-6" style="min-width: 400px;">
+          <img :src="require('@/assets/images/portfolio/logo design/IMG_20191029_194327_299.jpg')" class="" />
         </div>           
         
-        <div class="bg-white relative" style="min-width: 400px;">
-          <img :src="require('@/assets/images/portfolio/logo design/IMG_20191029_194327_299.jpg')" class="w-full" />
+        <div class="bg-white relative mx-6" style="min-width: 400px;">
+          <img :src="require('@/assets/images/portfolio/logo design/IMG_20191029_194327_299.jpg')" class="" />
         </div>           
         
-        <div class="bg-white relative" style="min-width: 400px;">
-          <img :src="require('@/assets/images/portfolio/logo design/IMG_20191029_194327_299.jpg')" class="w-full" />
+        <div class="bg-white relative mx-6" style="min-width: 400px;">
+          <img :src="require('@/assets/images/portfolio/logo design/IMG_20191029_194327_299.jpg')" class="" />
         </div>           
         
-        <div class="bg-white relative" style="min-width: 400px;">
-          <img :src="require('@/assets/images/portfolio/logo design/IMG_20191029_194327_299.jpg')" class="w-full" />
+        <div class="bg-white relative mx-6" style="min-width: 400px;">
+          <img :src="require('@/assets/images/portfolio/logo design/IMG_20191029_194327_299.jpg')" class="" />
         </div>   
       </VueSlickCarousel>
     </div>
 
     <div class="mt-20 ">
       <h1 class="text-4xl font-bold text-white flex justify-center mx-auto lg:text-6xl">Flyer design</h1>
-      <VueSlickCarousel :autoplay="true" :cssEase="ease" :pauseOnHover="true" :variableWidth="true" :arrows="false" :dots="false" :adaptiveHeight="true" class="gap-4 mt-8 w-full ">
+      <VueSlickCarousel :rtl="true" :autoplay="true" :cssEase="ease" :pauseOnHover="true" :variableWidth="true" :arrows="false" :dots="false" :adaptiveHeight="true" class="gap-4 mt-8 w-full ">
 
-        <div class="bg-white relative" style="width: 400px;">
+        <div class="bg-white relative mx-6" style="width: 400px;">
           <img :src="require('@/assets/images/portfolio/flyers/1st.jpg')" class="w-full" />
         </div>   
 
-        <div class="bg-white relative" style="width: 400px;">
+        <div class="bg-white relative mx-6" style="width: 400px;">
           <img :src="require('@/assets/images/portfolio/flyers/3.jpg')" class="w-full" />
         </div>        
         
-        <div class="bg-white relative" style="width: 400px;">
+        <div class="bg-white relative mx-6" style="width: 400px;">
           <img :src="require('@/assets/images/portfolio/flyers/Charmaine_SourceSmart_eflyerdesign_flatII_corrected_Upwork.jpg')" class="w-full" />
         </div>           
         
-        <div class="bg-white relative" style="width: 400px;">
+        <div class="bg-white relative mx-6" style="width: 400px;">
           <img :src="require('@/assets/images/portfolio/flyers/Ife2.jpg')" class="w-full" />
         </div>           
         
-        <div class="bg-white relative" style="width: 400px;">
+        <div class="bg-white relative mx-6" style="width: 400px;">
           <img :src="require('@/assets/images/portfolio/flyers/Raboni.jpg')" class="w-full" />
         </div>           
         
-        <div class="bg-white relative" style="width: 400px;">
+        <div class="bg-white relative mx-6" style="width: 400px;">
           <img :src="require('@/assets/images/portfolio/flyers/WA_September.jpg')" class="w-full" />
         </div>   
       </VueSlickCarousel>
